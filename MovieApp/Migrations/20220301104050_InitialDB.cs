6823a16f0f68ed6baaacc75e465d06dc9af9a8fd
Murat Sahin <Murat.sa25@gmail.com>
2022-03-03 10:07:59 +0100

    FEAT: Finished appendix A

---
+++ MovieApp/Migrations/20220301104050_InitialDB.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieApp.Migrations
+{
+    public partial class InitialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Character",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Alias = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    PictureUrl = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Character", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Franchise",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Franchise", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: true),
+                    ReleaseYear = table.Column<int>(type: "int", nullable: false),
+                    Director = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    PictureUrl = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Trailer = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    FranchiseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movie_Franchise_FranchiseId",
+                        column: x => x.FranchiseId,
+                        principalTable: "Franchise",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CharacterMovie",
+                columns: table => new
+                {
+                    CharactersId = table.Column<int>(type: "int", nullable: false),
+                    MoviesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterMovie", x => new { x.CharactersId, x.MoviesId });
+                    table.ForeignKey(
+                        name: "FK_CharacterMovie_Character_CharactersId",
+                        column: x => x.CharactersId,
+                        principalTable: "Character",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CharacterMovie_Movie_MoviesId",
+                        column: x => x.MoviesId,
+                        principalTable: "Movie",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Character",
+                columns: new[] { "Id", "Alias", "FullName", "Gender", "PictureUrl" },
+                values: new object[,]
+                {
+                    { 1, "Human", "Batman", "Male", "https://static.wikia.nocookie.net/batman/images/8/8f/Christian_Bale_as_The_Dark_Knight.jpg/revision/latest/scale-to-width-down/967?cb=20210312234853" },
+                    { 2, "Human", "Commissioner Gordon", "Male", "https://static.wikia.nocookie.net/batman/images/c/ca/Batman_photos_oldman.jpg/revision/latest/scale-to-width-down/300?cb=20080321214518" },
+                    { 3, "Human", "Bane", "Male", "https://static.wikia.nocookie.net/batman/images/f/f0/Bane_TDKR3.jpg/revision/latest/scale-to-width-down/470?cb=20120511112335" },
+                    { 4, "Hobbit", "Frodo Baggins", "Male", "https://static.wikia.nocookie.net/lotr/images/1/1a/FotR_-_Elijah_Wood_as_Frodo.png/revision/latest/scale-to-width-down/1000?cb=20130313174543" },
+                    { 5, "Maiar", "Saruman the White", "Male", "https://static.wikia.nocookie.net/lotr/images/0/0c/Christopher_Lee_as_Saruman.jpg/revision/latest?cb=20170127113833" },
+                    { 6, "Dunedain", "Aragorn", "Male", "https://static.wikia.nocookie.net/lotr/images/b/b6/Aragorn_profile.jpg/revision/latest?cb=20170121121423" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Franchise",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, null, "Batman Nolanverse" },
+                    { 2, null, "Lord of the Rings" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movie",
+                columns: new[] { "Id", "Director", "FranchiseId", "Genre", "PictureUrl", "ReleaseYear", "Title", "Trailer" },
+                values: new object[,]
+                {
+                    { 1, "Christopher Nolan", 1, "Action", "", 2005, "Batman Begins", "" },
+                    { 2, "Christopher Nolan", 1, "Action", "", 2008, "The Dark Knight", "" },
+                    { 3, "Christopher Nolan", 1, "Action", "", 2012, "The Dark Knight Rises", "" },
+                    { 4, "Peter Jackson", 2, "Fantasy", "", 2001, "The Fellowship of the Ring", "" },
+                    { 5, "Peter Jackson", 2, "Fantasy", "", 2003, "The Two Towers", "" },
+                    { 6, "Peter Jackson", 2, "Fantasy", "", 2005, "The Return of the King", "" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterMovie_MoviesId",
+                table: "CharacterMovie",
+                column: "MoviesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movie_FranchiseId",
+                table: "Movie",
+                column: "FranchiseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CharacterMovie");
+
+            migrationBuilder.DropTable(
+                name: "Character");
+
+            migrationBuilder.DropTable(
+                name: "Movie");
+
+            migrationBuilder.DropTable(
+                name: "Franchise");
+        }
+    }
+}
